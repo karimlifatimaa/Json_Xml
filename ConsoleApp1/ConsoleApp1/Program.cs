a1139df4a0ea4770b58ac20b4ed8b3b1e1f26d70
karimlifatimaa <bd7tzqw95@code.edu.az>
2024-04-22 23:44:58 +0400

    Jason_Xml

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -20,12 +20,23 @@ namespace ConsoleApp1
             // JSON DeSerialize
             Product deserializedProduct = JsonSerializer.Deserialize<Product>(json);
             Console.WriteLine("Deserialized Product - Name: " + deserializedProduct.Name + ", Price: " + deserializedProduct.Price);
-
+            Console.WriteLine("-------------------------------------------------");
             //XML Serialize
             XmlSerializer xmlSerializer = new XmlSerializer(typeof(Product));
-            using StringWriter writer = new StringWriter();
-            xmlSerializer.Serialize(writer, product);
-            Console.WriteLine(writer.ToString());
+            using (StringWriter writer = new StringWriter())
+            {
+                xmlSerializer.Serialize(writer, product);
+                string xml = writer.ToString();
+                Console.WriteLine("Serialized XML:");
+                Console.WriteLine(xml);
+
+                // XML DeSerialize
+                using (StringReader reader = new StringReader(xml))
+                {
+                    Product deserializedProductXml = (Product)xmlSerializer.Deserialize(reader);
+                    Console.WriteLine("Deserialized Product from XML - Name: " + deserializedProductXml.Name + ", Price: " + deserializedProductXml.Price);
+                }
+            }
 
         }
     }
--- ConsoleApp1/ConsoleApp1/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net6.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d454b935a6a731d8f133f58aaa2e7711b2747ee9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
