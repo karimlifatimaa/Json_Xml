d454b935a6a731d8f133f58aaa2e7711b2747ee9
karimlifatimaa <bd7tzqw95@code.edu.az>
2024-04-22 23:28:54 +0400

    Json,Xml

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using System;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // JSON Serialize
+            Product product = new Product 
+            { 
+                Name = "Alma", 
+                Price = 20 
+            };
+            string json = JsonSerializer.Serialize(product);
+            Console.WriteLine("Serialized JSON: " + json);
+
+            // JSON DeSerialize
+            Product deserializedProduct = JsonSerializer.Deserialize<Product>(json);
+            Console.WriteLine("Deserialized Product - Name: " + deserializedProduct.Name + ", Price: " + deserializedProduct.Price);
+
+            //XML Serialize
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Product));
+            using StringWriter writer = new StringWriter();
+            xmlSerializer.Serialize(writer, product);
+            Console.WriteLine(writer.ToString());
+
+        }
+    }
+}
